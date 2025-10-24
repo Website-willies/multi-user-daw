56d17bbf29cc1d650e215bdc56b94b072aa49981
Liam Ostrander <liamostrander0@gmail.com>
2025-10-23 23:07:24 -0400

    added basic directory structure, pg and node-pg-migrate to package lock, as well as a few sample scripts in the package file

---
+++ src/server.js
@@ -0,0 +1,13 @@
+let express = require("express");
+let { Pool } = require("pg");
+
+let hostname = "localhost";
+let port = 3000;
+let app = express();
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.listen(port, hostname, () => {
+  console.log(`http://${hostname}:${port}`);
+});
