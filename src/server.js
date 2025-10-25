1811e1c7a5c57f19f37a7bbc60bb24861e788a71
eb122333 <131624512+eb122333@users.noreply.github.com>
2025-10-24 21:42:52 -0400

    fix script, add uuid/session

--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 import express from "express";
 import pool from "./db/pool.js";
 let app = express();
+
 let hostname = "localhost";
 let port = 3000;
+
+//TODO: move uuid storage from in-memory to DB
+//Want to discuss usage and schema first
+
+let uuids = {}
+
 app.use(express.json());
 app.use(express.static("public"));
+
 app.use("/sounds", express.static("server/sounds"));
 
 app.get("/sounds", async (req, res) => {
@@ -15,6 +23,16 @@ app.get("/sounds", async (req, res) => {
     console.error("Error fetching sounds:", err);
     res.status(500).json({ error: "Internal Server Error" });
   }
+});
+
+app.get("/assign", (req, res) => {
+
+  res.status(200);
+  res.contentType('application/json');
+  let id = crypto.randomUUID()
+  uuids[id] = "empty"
+  res.json({ "uuid": id });
+
 });
 
 app.listen(port, hostname, () => {
