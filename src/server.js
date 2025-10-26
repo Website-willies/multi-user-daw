e043f32f859c24d0e31d9cc4144b49824d053146
Liam Ostrander <liamostrander0@gmail.com>
2025-10-26 13:14:47 -0400

    added readme for setup and endpoint to check sounds db in server.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
-let express = require("express");
-let { Pool } = require("pg");
-
+import express from "express";
+import pool from "./db/pool.js";
+let app = express();
 let hostname = "localhost";
 let port = 3000;
-let app = express();
-
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/sounds", express.static("server/sounds"));
+
+app.get("/sounds", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM sounds ORDER BY id;");
+    res.json(result.rows);
+  } catch (err) {
+    console.error("Error fetching sounds:", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 app.listen(port, hostname, () => {
   console.log(`http://${hostname}:${port}`);
