50b537776c5ad0a397f7472db33d65742831832f
Liam Ostrander <liamostrander0@gmail.com>
2025-11-05 20:11:20 -0500

    added volume sliders, western pitch scaling, and a few more instruments

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 import express from "express";
+import fs from 'fs';
 import path from "path";
-import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import soundRoutes from "./routes/sounds.js";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 const oneshotsPath = path.join(__dirname, "oneshots");
 
 let app = express();
@@ -20,6 +20,17 @@ app.use("/oneshots", express.static(oneshotsPath));
 
 app.get("/assign", (req, res) => {
   res.status(200).json({ "uuid": crypto.randomUUID() })
+});
+
+app.get('/list-oneshots', (req, res) => {
+  let oneshot_names = [];
+  fs.readdir(oneshotsPath, (err, files) => {
+    if (err) return res.status(500).json({ error: err.message });
+    for (let file of files){
+      oneshot_names.push(path.parse(file).name)
+    }
+    res.json(oneshot_names);
+  });
 });
 
 app.listen(port, hostname, () => {
