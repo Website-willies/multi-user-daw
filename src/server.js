6ce96084e7b895d32dedf75d32de70c8df8b6ecf
Kaleb <104028332+kaleblefko@users.noreply.github.com>
2025-11-19 16:47:38 -0500

    websocket integration completed.

--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import fs from 'fs';
 import path from "path";
 import { fileURLToPath } from 'url';
+import http from 'http';
+
+import { createWebSocketServer } from "./routes/ws.js";
 import soundRoutes from "./routes/sounds.js";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -9,13 +12,15 @@ const __dirname = path.dirname(__filename);
 const oneshotsPath = path.join(__dirname, "oneshots");
 
 let app = express();
+let server = http.createServer(app);
+let wss = createWebSocketServer(server);
 let hostname = "0.0.0.0";
 let port = 3000;
 
 app.use(express.json());
 app.use(express.static("src/public"));
 
-app.use("/sounds", soundRoutes);
+app.use("/sounds", soundRoutes(wss));
 app.use("/oneshots", express.static(oneshotsPath));
 
 app.get("/assign", (req, res) => {
@@ -41,6 +46,6 @@ app.get("/list-oneshots", (req, res) => {
   }
 });
 
-app.listen(port, hostname, () => {
+server.listen(port, hostname, () => {
   console.log(`http://${hostname}:${port}`);
 });
